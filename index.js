9fc1beea498ec5b4558186aaa386ba881ab61e89
84mb <103484634+84mb@users.noreply.github.com>
2022-04-12 17:26:24 +1000

    index.js
    All the code as of 12th of April 2022

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+
+const { MessageEmbed } = require('discord.js');
+
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+process.setMaxListeners(0);
+
+//const client = new Discord.Client();
+let prefix = ";";
+
+client.on("messageCreate", message =>{
+    if(!message.content.startsWith(prefix+"code")){return;}
+    let tag = message.member.user.id;
+    console.log(";code command");
+    message.channel.send(" Hey <@"+tag+">! \n \n I am a discord bot coded by 84mb#1535. I am made for Tritho's discord server! \n \n You can access my code on GitHub, just search up 84mb and you should see! (I can't send links here)");
+});
+
+client.on("messageCreate", message =>{
+    //i know i can combine these if statements but we dont like efficiency here
+    if(message.channel.id == "963332817549414400"){
+      if(message.content.indexOf(" ")!==-1){
+        message.channel.bulkDelete(1);
+       }
+    }
+});
+
+
+client.login("has been removed for obvious reasons")
